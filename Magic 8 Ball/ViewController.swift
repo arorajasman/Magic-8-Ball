80e32be0632c91f16f072828127d5266f619020a
arorajasman <arorajasman97@gmail.com>
2022-08-09 21:33:49 +0530

    Added the code for the UI of the app
    Added the logic for randomly changing the image of the answer ball when the ask button is pressed
    Completed the app

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    // the code below is used to create an array that
+    // stores the list of names of images of balls
+    let ballArray: [String] = ["ball1","ball2","ball3","ball4","ball5"]
 
 
-
+    // the code below is used to create an IBOutlet for the
+    // image ball that will be used to answer the queries
+    // made by the user
+    @IBOutlet weak var answerBallimageView: UIImageView!
+    
+    
+    // the code below is used to create a method
+    // of type IBAction that will be called when
+    // the ask button is pressed
+    @IBAction func askButtonOnPressedHandler(_ sender: UIButton) {
+        
+        // the code is used to create a property for getting the random index value to show the
+        // ransom images of the ball from the ballArray
+        let randomIndex = Int.random(in: 0 ... 4)  // here we are using the random() method from the
+        // Int class for getting a random index value between 0 and 4 including both 0 and 4 as well
+        
+        // the below line of code is for debugging purpose
+        print("Ask button Pressed")
+        
+        // the below line of code is for debugging purpose
+        print("the current value of random index is: \(randomIndex)")
+        
+        // the code below is to use the image property of the answerBallimageView variable for
+        // changing the image of the answer ball when the ask button is clicked
+        // answerBallimageView.image = UIImage(named: ballArray[2])  // here we are using the UIImage()
+        // object and passing the name of the image as a value to the named variable for showing
+        // the image of the ball at index of the ballArray when the ask button is pressed
+        
+        
+        // the code below is to use the randomIndex value for getting the image name at randomIndex
+        // for ballArray for displaying the random images of the ball when the ask button is clicked
+        answerBallimageView.image = UIImage(named: ballArray[randomIndex])  // here we are using the UIImage()
+        // object and passing the name of the image as a value to the named variable for showing
+        // the image of the ball at index of the ballArray when the ask button is pressed
+    }
 }
 
